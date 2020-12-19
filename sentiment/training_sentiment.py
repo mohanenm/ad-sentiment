2862b0e931aa94e9133ca2bdb7f31f2ff4a667f9
Ethan Mohan <ethannmohan@gmail.com>
2020-12-19 07:45:38 -0500

    removed keys

--- sentiment/training_sentiment.py
+++ sentiment/training_sentiment.py
@@ -1,7 +1,8 @@
 import nltk
-from nltk.corpus import twitter_samples
+from nltk.corpus import twitter_samples, stopwords
 from nltk.tag import pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
+import re, string
 
 # using nltk.tag for positve markings here
 # using averaged percepton tagger for token context within sentence
@@ -12,6 +13,7 @@ text = twitter_samples.strings('tweets.20150430-223406.json')
 # grabing a little data at first, for the purpose of visualizing it.
 # Ultimatly training will happen in a larger context
 tweet_tokens = twitter_samples.tokenized('positive_tweets.json')[0:1]
+
 
 # print(pos_tag(tweet_tokens[0]))
 
@@ -32,10 +34,48 @@ def lemmatize_sentence(tokens):
         lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
     return lemmatized_sentence
 
-print(lemmatize_sentence(tweet_tokens[0]))
-
-
 # regex here for similar reasons as lemmatization -->
 # a lot of the data I am getting in does nothing for me
 # I want the simplest version of word to train the model on
 # not links, not numbers, etc
+# does not have to be so high up, might move down later
+# just nice to look at right now....
+stop_wrds = stopwords.words('english')
+
+
+def rmv_noise(tweet_tokens, stop_wrds=()):
+    cleaned_tokens = []
+
+    for token, tag in pos_tag(tweet_tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|' \
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token)
+        token = re.sub("(@[A-Za-z0-9_]+)", "", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_wrds:
+            cleaned_tokens.append(token.lower())
+    return cleaned_tokens
+
+
+positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
+
+positive_cleaned_tokens_list = []
+negative_cleaned_tokens_list = []
+
+for tokens in positive_tweet_tokens:
+    positive_cleaned_tokens_list.append(rmv_noise(tokens, stop_wrds))
+
+for tokens in negative_tweet_tokens:
+    negative_cleaned_tokens_list.append(rmv_noise(tokens, stop_wrds))
+
+
